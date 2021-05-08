580b725265cc9608568c808e7f3e6eb64151ac42
Roig <groig@protonmail.com>
2021-05-08 19:12:52 -0400

    add contex lib

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Urtstats.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:contex, "~> 0.3.0"}
     ]
   end
 
